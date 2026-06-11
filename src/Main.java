77d73c3ef9828463b52e8639fb5b4c5d3b9075a3
txlu-dev <mrtawosanya@outlook.com>
2026-06-11 15:27:41 +0100

    Added ability to mark tasks as complete

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,8 @@ public class Main {
             System.out.println("\n=== Revision Planner ===");
             System.out.println("1. Add Task");
             System.out.println("2. View Tasks");
-            System.out.println("3. Exit");
+            System.out.println("3. Complete Task");
+            System.out.println("4. Exit");
             System.out.print("Choice: ");
             
             
@@ -39,19 +40,61 @@ public class Main {
                 // Display all tasks
                 case 2:
                     System.out.println("\nTasks:");
-
+                    
+                    //Check if tasks are empty
                     if (tasks.isEmpty()) {
-                        System.out.println("No tasks.");
-                    } else {
+                        System.out.println("No tasks."); } 
+                        
+                    else {
                         for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println((i + 1) + ". "
-                                    + tasks.get(i).getTitle());
+                        	
+                        	// Check if task is completed
+                        	if (tasks.get(i).getComplete()) {
+                        		System.out.println((i + 1) + ". "+"[X] "
+                                        + tasks.get(i).getTitle());}
+                        	else {
+                        		System.out.println((i + 1) + ". "+"[ ] "
+                                        + tasks.get(i).getTitle());}
                         }
                     }
+                    
+                   
                     break;
                     
-                // Exit
+                    
+                    
                 case 3:
+                    System.out.println("\nTasks:");
+                    
+                    //Check if tasks are empty
+                    if (tasks.isEmpty()) {
+                        System.out.println("No tasks."); } 
+                        
+                    else {
+                        for (int i = 0; i < tasks.size(); i++) {
+                        	
+                        	// Check if task is completed
+                        	if (tasks.get(i).getComplete()) {
+                        		System.out.println((i + 1) + ". "+"[X] "
+                                        + tasks.get(i).getTitle());}
+                        	else {
+                        		System.out.println((i + 1) + ". "+"[ ] "
+                                        + tasks.get(i).getTitle());}
+                        }
+                    }
+                    
+                    System.out.print("Enter task number: ");
+                    int taskNum = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    tasks.get(taskNum - 1).markComplete();
+                    
+                   
+                    break;                    
+                   
+                    
+                // Exit
+                case 4:
                     System.out.println("Goodbye!");
                     break;
 
@@ -59,7 +102,7 @@ public class Main {
                     System.out.println("Invalid choice.");
             }
 
-        } while (choice != 3);
+        } while (choice != 4);
 
         scanner.close();
     }
